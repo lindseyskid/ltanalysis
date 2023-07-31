d1decde29ea45d8e174a28b957b1af4b499905f2
lindseyskid <139925350+lindseyskid@users.noreply.github.com>
2023-07-31 15:01:12 -0700

    Update GITHUB.R

--- GITHUB.R
+++ GITHUB.R
@@ -13,52 +13,52 @@ library(spatstat)
 #howdy!
 
 #import data
-lt <- brick("NWCasc_Lindsey/Data Inputs/Middle_data/lt-gee_growth_map(2).tif")
-chm <- raster("NWCasc_Lindsey/Data Inputs/scratch_folder/CHM_with_Buffer1.tif")
-meadows <- st_read("Wetland_MeadowID/input_data/MORA polygons/MORA_meadows.shp")
+spatrast <- rast("NWCasc_Lindsey/Data Inputs/scratch_folder/CHM_with_Buffer1.tif", subds = 0, lyrs = NULL, drivers = NULL, opts = NULL, win = NULL, snap = "near", vsi = FALSE)
+ltspatrast <- rast("NWCasc_Lindsey/Data Inputs/Middle_data/lt-gee_changemap.tif", lyrs = "mag")
+DEM <- rast("Wetland_MeadowID/intermediate_data/RF_Model_Input/DEM.tif")
+NDVI <- rast("NWCasc_Lindsey/Data Inputs/scratch_folder/NDVIground7.tif")
 
 #change projection
 epsg_code <- 4269
-crs(chm) <- CRS(paste0("+init=EPSG:", epsg_code))
-crs(lt) <- CRS(paste0("+init=EPSG:", epsg_code))
-st_crs(meadows) <- 4269
-projection(chm)
-projection(lt)
-projection(meadows)
+ltspatrast <- project(ltspatrast, "epsg:26910")
+spatrast <- project(spatrast, "epsg:26910")
+DEM <- project(DEM, "epsg:26910")
+NDVI <- project(NDVI, "epsg:26910")
 
-#plot 
-plot(chm, main = "Canopy Height Model")
-plot(meadows, add = TRUE)
-plot(lt[[2]], main = "Magnitude")
-print(lt)
-
-#change raster to Spatraster
-?rast
-spatrast <- rast("NWCasc_Lindsey/Data Inputs/scratch_folder/CHM_with_Buffer1.tif", subds = 0, lyrs = NULL, drivers = NULL, opts = NULL, win = NULL, snap = "near", vsi = FALSE)
-plot(spatrast)
-print(spatrast)
-ltspatrast <- rast("NWCasc_Lindsey/Data Inputs/Middle_data/lt-gee_growth_map(2).tif", subds = "", lyrs = 2, drivers = NULL, opts = NULL, win = NULL, snap = "near", vsi = FALSE)
-plot(ltspatrast)
-print(ltspatrast)
+crs(ltspatrast)
+crs(spatrast)
+crs(DEM)
+crs(NDVI)
 
 #mask for classes
-groundrastmask <- spatrast == 0
+groundrastmask <- as.numeric(NDVI == 1)
+groundrastmask[groundrastmask != 1] <- NA
 plot(groundrastmask, main = "ground")
-meadowrastmask <- spatrast == 1
+
+meadowrastmask <- as.numeric(spatrast == 1)
+meadowrastmask[meadowrastmask != 1] <- NA
 plot(meadowrastmask, main = "meadow")
-shrubrastmask <- spatrast == 2
+
+shrubrastmask <- as.numeric(spatrast == 2)
+shrubrastmask[shrubrastmask != 1] <- NA
 plot(shrubrastmask, main = "shrub")
-treedrastmask <- spatrast == 3
+
+treedrastmask <- as.numeric(spatrast == 3)
+treedrastmask[treedrastmask != 1] <- NA
 plot(treedrastmask, main = "treed")
 
 #random points
 ?spatSample
-randompointsground <- spatSample(groundrastmask, 2500, method = "stratified", replace = FALSE, na.rm = FALSE, as.raster = FALSE, as.df = FALSE, as.points = TRUE, values = TRUE, cells = FALSE, xy = TRUE, ext = NULL, warn = TRUE, weights = NULL, exp = 3, exhaustive = FALSE)
-randompointsmeadow <- spatSample(meadowrastmask, 2500, method = "stratified", replace = FALSE, na.rm = FALSE, as.raster = FALSE, as.df = FALSE, as.points = TRUE, values = TRUE, cells = FALSE, xy = TRUE, ext = meadowrastmask, warn = TRUE, weights = NULL, exp = 3, exhaustive = FALSE)
-randompointsshrub <- spatSample(groundrastmask, 2500, method = "stratified", replace = FALSE, na.rm = FALSE, as.raster = FALSE, as.df = FALSE, as.points = TRUE, values = TRUE, cells = FALSE, xy = TRUE, ext = shrubrastmask, warn = TRUE, weights = NULL, exp = 3, exhaustive = FALSE)
-randompointstreed <- spatSample(groundrastmask, 2500, method = "stratified", replace = FALSE, na.rm = FALSE, as.raster = FALSE, as.df = FALSE, as.points = TRUE, values = TRUE, cells = FALSE, xy = TRUE, ext = treedrastmask, warn = TRUE, weights = NULL, exp = 3, exhaustive = FALSE)
+randompointsground <- spatSample(groundrastmask, 2500, method = "stratified", replace = FALSE, na.rm = TRUE, as.raster = FALSE, as.df = TRUE, as.points = TRUE, values = FALSE, cells = FALSE, xy = TRUE, ext = NULL, warn = TRUE, weights = NULL, exp = 3, exhaustive = FALSE)
+randompointsmeadow <- spatSample(meadowrastmask, 2500, method = "stratified", replace = FALSE, na.rm = TRUE, as.raster = FALSE, as.df = FALSE, as.points = TRUE, values = FALSE, cells = FALSE, xy = TRUE, ext = NULL, warn = TRUE, weights = NULL, exp = 3, exhaustive = FALSE)
+randompointsshrub <- spatSample(shrubrastmask, 2500, method = "stratified", replace = FALSE, na.rm = TRUE, as.raster = FALSE, as.df = FALSE, as.points = TRUE, values = FALSE, cells = FALSE, xy = TRUE, ext = NULL, warn = TRUE, weights = NULL, exp = 3, exhaustive = FALSE)
+randompointstreed <- spatSample(treedrastmask, 2500, method = "stratified", replace = FALSE, na.rm = TRUE, as.raster = FALSE, as.df = FALSE, as.points = TRUE, values = FALSE, cells = FALSE, xy = TRUE, ext = NULL, warn = TRUE, weights = NULL, exp = 3, exhaustive = FALSE)
 
 #plot points
+plot(ltspatrast)
+print(ltspatrast)
+plot(spatrast)
+
 plot(randompointsground, add = TRUE, cex = 0.1)
 print(randompointsground)
 
@@ -71,24 +71,21 @@ print(randompointsshrub)
 plot(randompointstreed, add = TRUE, cex = 0.1)
 print(randompointstreed)
 
-#extract values from points
-extractvaluesground <- terra::extract(ltspatrast, randompointsground, fun = NULL, method = "simple")
+#extract MAGNITUDE values from points
+?extract
+extractvaluesground <- terra::extract(ltspatrast, randompointsground, fun = NULL, method = "simple", bind = TRUE)
 summary(extractvaluesground)
 
-extractvaluesmeadow <- terra::extract(ltspatrast, randompointsmeadow, fun = NULL, method = "simple")
+extractvaluesmeadow <- terra::extract(ltspatrast, randompointsmeadow, fun = NULL, method = "simple", bind = TRUE)
 summary(extractvaluesmeadow)
 
-
-extractvaluesshrub <- terra::extract(ltspatrast, randompointsshrub, fun = NULL, method = "simple")
+extractvaluesshrub <- terra::extract(ltspatrast, randompointsshrub, fun = NULL, method = "simple", bind = TRUE)
 summary(extractvaluesshrub)
 
-extractvaluestreed <- terra::extract(ltspatrast, randompointstreed, fun = NULL, method = "simple")
+extractvaluestreed <- terra::extract(ltspatrast, randompointstreed, fun = NULL, method = "simple", bind = TRUE)
 summary(extractvaluestreed)
-print(extractvaluestreed)
 
 #box plot 
-?boxplot
-
 magnitude_ground <- extractvaluesground$mag
 magnitude_meadow <- extractvaluesmeadow$mag
 magnitude_shrub <- extractvaluesshrub$mag
@@ -112,6 +109,12 @@ boxplot(extracted_magnitude_list,
         ylim = c(0,1700),
         border = "darkblue", 
         pch = 1700)             
+
+#Bring points into arc 
+write.csv(extractvaluesground, "groundpoints.csv")
+write.csv(extractvaluesmeadow, "meadowpoints.csv")
+write.csv(extractvaluesshrub, "shrubpoints.csv")
+write.csv(extractvaluestreed, "treedpoints.csv")
 
 
 
